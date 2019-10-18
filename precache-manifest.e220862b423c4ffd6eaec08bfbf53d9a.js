765741751c5e8138ebae4220bfbafe8351b4043f
Sergii Stryzg <strizgsergey@gmail.com>
2019-10-18 11:54:34 +0300

    Updates

---
+++ precache-manifest.e220862b423c4ffd6eaec08bfbf53d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66376f776bee753582e8274c9693b96f",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "ae3e1dd75ce7204a0c9a",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f83384252826bf345fa3",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.1898ec51.chunk.css"
+  },
+  {
+    "revision": "ae3e1dd75ce7204a0c9a",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.dc1eef05.chunk.js"
+  },
+  {
+    "revision": "f83384252826bf345fa3",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.8a9f042f.chunk.js"
+  },
+  {
+    "revision": "a898c5ccd6cf1bcbc60a",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.1775bb0a.js"
+  }
+]);
